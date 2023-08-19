b0512cc2664d52a7fd92ce9e643153ba9a07d837
Sara Tamer <S320022@ese.gov.ae>
2023-08-20 01:55:24 +0300

    juikbhi

--- cat/CatFactsView.swift
+++ cat/CatFactsView.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct CatFactsView: View {
+    
+@StateObject var catFaactsManager =
+    CatFactsManager()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .onAppear {
+                .catFactsManager.getCatFact()
+            }
     }
 }
 
