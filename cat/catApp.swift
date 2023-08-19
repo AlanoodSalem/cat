ad4edb112628b861e1d1f325d0d181bc4f400980
Alanood <s167959@ese.gov.ae>
2023-08-20 01:44:31 +0400

    Initial Commit

---
+++ cat/catApp.swift
@@ -0,0 +1,17 @@
+//
+//  catApp.swift
+//  cat
+//
+//  Created by Alanood Almansoori on 20/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct catApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
