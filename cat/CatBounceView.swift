83e645943a4d3ec02c7f1f14d5125fa4c8f3e4b9
Rana emad <s241172@ese.gov.ae>
2023-08-20 02:15:05 +0400

    cat

---
+++ cat/CatBounceView.swift
@@ -0,0 +1,20 @@
+//
+//  CatBounceView.swift
+//  cat
+//
+//  Created by Rana Emad on 20/08/2023.
+//
+
+import SwiftUI
+
+struct CatBounceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CatBounceView_Previews: PreviewProvider {
+    static var previews: some View {
+        CatBounceView()
+    }
+}
---
+++ cat/CatGalleryView.swift
@@ -0,0 +1,22 @@
+//
+//  CatGalleryView.swift
+//  cat
+//
+//  Created by Rana Emad on 20/08/2023.
+//
+
+import SwiftUI
+
+struct CatGalleryView: View {
+    var body: some View {
+         Text("iuygiuh")
+        
+    }
+}
+struct CatGalleryView_Previews: PreviewProvider
+{
+    static var previews: some View {
+        CatGalleryView()
+    }
+
+}
